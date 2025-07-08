7b98f528ad70e30f10003c7e9dc1b6b467e83723
ming.guo <ming.guo@sunmi.com>
2025-07-08 19:08:17 +0800

    update func

--- app/dal/page.go
+++ app/dal/page.go
@@ -26,14 +26,15 @@ func (*pageImpl) Create(ctx context.Context, page *model.Page) error {
 }
 
 // Get 可以通过多种id获取页面
-func (*pageImpl) Get(ctx context.Context, pageId string, pageIdType PageIdType) (*model.Page, error) {
+func (*pageImpl) Get(ctx context.Context, uid int64, pageId string, pageIdType PageIdType) (*model.Page, error) {
+	// 这里缺少一些权限校验，比如是否是该页面的owner，是否是该页面的readonly，是否是该页面的edit，是否是该页面的admin
 	u := Q.Page
 	do := u.WithContext(ctx)
 	switch pageIdType {
 	case DefaultPageIdType:
-		do = do.Where(u.Pid.Eq(pageId))
+		do = do.Where(u.Pid.Eq(pageId), u.UID.Eq(uid)) // 默认页面，需要是该页面的owner
 	case ReadonlyPageIdType:
-		do = do.Where(u.ReadonlyPid.Eq(pageId))
+		do = do.Where(u.ReadonlyPid.Eq(pageId)) // 只读页面，谁都能访问
 	case EditPageIdType:
 		do = do.Where(u.EditPid.Eq(pageId))
 	case AdminPageIdType:
@@ -67,7 +68,7 @@ func (*pageImpl) GetPages(ctx context.Context, uid int64, ownerIds, readonlyIds,
 		}
 	}
 
-	if len(readonlyIds) > 0 {
+	if len(editIds) > 0 {
 		edit, err = do.Where(u.UID.Eq(uid), u.EditPid.In(editIds...)).Find()
 		if err != nil {
 			err = transGormErr(err)
@@ -91,15 +92,18 @@ func (*pageImpl) DeleteByPid(ctx context.Context, uid int64, pid string) error {
 
 // UnlinkPage 解除页面链接
 // 不真删，只是把该页面链接移除
+// 需要时该页面的人，才有权限移除
 func (*pageImpl) UnlinkPage(ctx context.Context, uid int64, readOnlyPid, editPid, adminPid string) error {
 	u := Q.Page
 	do := u.WithContext(ctx)
 	do = do.Where(u.UID.Eq(uid))
 	var err error
 	if readOnlyPid != "" {
-		_, err = do.Where(u.ReadonlyPid.Eq(readOnlyPid)).UpdateSimple(u.ReadonlyPid, "")
+		_, err = do.Where(u.ReadonlyPid.Eq(readOnlyPid)).UpdateSimple(u.ReadonlyPid.Value(""))
 	} else if editPid != "" {
-
+		_, err = do.Where(u.EditPid.Eq(editPid)).UpdateSimple(u.EditPid.Value(""))
+	} else if adminPid != "" {
+		_, err = do.Where(u.AdminPid.Eq(adminPid)).UpdateSimple(u.AdminPid.Value(""))
 	}
-
+	return transGormErr(err)
 }
